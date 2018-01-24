878366d3eee70b24743254d608f247cb7eec961a
alex <a2holley8@gmail.com>
2018-01-23 22:15:16 -0600

    Updated .gitignore to ignore all json files

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+############################
+# TEST VERSION
+############################
+
 import discord
 import asyncio
 import json
@@ -10,6 +14,7 @@ from affix import affixes
 client = discord.Client()
 officersID = ['204420413814472704']
 
+prefix = '!'
 
 @client.event
 async def on_ready():
@@ -22,88 +27,31 @@ async def on_ready():
 async def on_message(message):
     global officersID
 
-    if message.content.startswith('!thelp'):
-        await client.send_message(message.channel, '```Available TEST commands for this Channel:' 
-            '\n!tselect - Test version of random member select.'
-            '\n\tThis randomly selects a member from a pre-defined channel'
-            '\n!tofficer - test version of officer only commands'
-            '\n\tThis command only works for users with a certain role```'
-            )
+    roles = []
 
-## Available to specified Roles ONLY
-# 380763363908648963 - Test_Role
- 
-    #if '380763363908648963' in (y.id for y in message.author.roles):
+    print(message.author.roles)
 
-        ## These are the test version commands
-        # await client.send_message(message.channel, "```Officer Command Invoked```")
-    if (message.content.startswith('!tofficer')) and ('380763363908648963' in (y.id for y in message.author.roles)) :
-        await client.delete_message(message)
-        await client.send_message(message.channel, '```Confirmed```')
-        ## Test version END
+    for role in message.author.roles:
+        roles.append(role.name)
 
-    elif (message.content.startswith('!tup')) and ('380763363908648963' in (y.id for y in message.author.roles)):
-        for x in officersID:
-            # Moves everyone in officersID list to Officer channel 
-            await client.move_member(client.get_server('336532282678575104').get_member(x), client.get_channel('336532282678575106'))
-        await client.delete_message(message)
+    print(roles)
+    if 'Test_Role' in roles:
+        print('yup, its there')
 
-    elif (message.content.startswith('!tdown')) and ('380763363908648963' in (y.id for y in message.author.roles)) :
-        for x in officersID:
-            # Moves everyone in officersID list to Raid channel 
-            await client.move_member(client.get_server('336532282678575104').get_member(x), client.get_channel('380762592400113665'))
-        await client.delete_message(message)
+    if message.author == client.user:
+        return
 
+    if message.content.startswith(prefix):
+        print(message.content)                  # print our initial command string
+        terms = message.content[1:].split()     # strips the leading ! and splits the command string
+        command = terms[0].lower()              # pulls the first term, aka the command, and casts to lower
+        print(command)                          # verify we have the first term by printing it
+        if len(terms) > 1:
+            for x in terms[1:]:
+                print(x)                        # print the next terms
+        
 
-## Available to all Roles :)
-
-    elif message.content.startswith('!tselect') and ('380763363908648963' in (y.id for y in message.author.roles)):
-        officersID = ['204420413814472704']
-        print('officersID before select:')
-        print(officersID)
-        #await client.send_message(message.channel, message.author.id)  ## Debugging
-        #members = client.get_channel('336532282678575106').voice_members  
-        members = message.author.voice.voice_channel.voice_members
-        # returns a list of members in the specified channel
-        print('Length of members list: ' + str(len(members)))  ## Debugging
-        # Check to see if there are users before trying to select one
-        if (len(members) > 0):
-            ran_mem = random.randint(0,len(members)-1)
-            #print('ran_mem: ' + str(ran_mem))  ## Debugging
-            #print(members[ran_mem].id)  ## Debugging
-            #if (members[ran_mem].id not in officersID):
-                #print('chosen: ' + str(members[ran_mem].name)) 
-                #await client.send_message(message.channel, str(members[ran_mem].name)+' has been choosen!')
-                #officersID.append(str(members[ran_mem].id))
-                #print(officersID)
-
-            # This loop will be active as long as the selected person is an officer
-            # it will keep getting a new random int to select a new person until the new person is
-            # not in officersID. If also includes a debugging print statement
-            while (members[ran_mem].id in officersID):
-                print('ran_mem: ' + str(ran_mem))               ## Debugging: print the current random int
-                print('chosen: ' + str(members[ran_mem].name))  ## Debugging: print who was chosen
-                ran_mem = random.randint(0,len(members)-1)     # get new random int
-            # Out of loop, selected member must NOT be in officerID already
-            await client.send_message(message.channel, str(members[ran_mem].nick) + ' had been choosen!')
-            officersID.append(str(members[ran_mem].id))
-            print(officersID)   ## Debugging
-        # END IF
-
-    elif message.content.startswith('!treset') and ('380763363908648963' in (y.id for y in message.author.roles)):
-        officersID = ['204420413814472704']
-        await client.send_message(message.channel, '```Reset Complete```')
-        print(officersID)
-
-        #for x in gen.voice_members:
-            #await client.send_message(message.channel, 'Discord RealID: '+ str(x) + ' Discord Identifier: ' + str(x.id) + ' Username: ' + str(x.name))
-            #print('Discord RealID: '+ str(x) + ' Discord Identifier: ' + str(x.id) + ' Username: ' + str(x.name))
-            #if x.name == 'Tuggy':
-                #await client.send_message(message.channel, 'Hello Tuggy :)')
-
-#END IF
-
-client.run('MzgwNDMyMjQ5MzgxOTc4MTIy.DO4g_Q.6Up4UAH6VNhCnSB-osdRiQVYhM0')
+client.run('MzgwNDMyMjQ5MzgxOTc4MTIy.DUc9dQ.WU-NbNx6M-GN0xOhsx4bdryRUBg')
 
 #END FILE
 
